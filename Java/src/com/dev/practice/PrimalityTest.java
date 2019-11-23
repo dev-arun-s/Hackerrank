d87f0999f8b45a62b5a527a4772abe1641942430
dev-arun-s <57510415+dev-arun-s@users.noreply.github.com>
2019-11-23 18:54:49 -0500

    Update PrimalityTest.java

--- Java/src/com/dev/practice/PrimalityTest.java
+++ Java/src/com/dev/practice/PrimalityTest.java
@@ -3,33 +3,13 @@ package com.dev.practice;
 import java.math.BigInteger;
 import java.util.Scanner;
 
-/*
- * A prime number is a natural number greater than  whose only positive divisors are  and itself. For example, the first six prime numbers are , , , , , and .
 
-Given a large integer, , use the Java BigInteger class' isProbablePrime method to determine and print whether it's prime or not prime.
-
-Input Format
-
-A single line containing an integer,  (the number to be checked).
-
-Constraints
-
- contains at most  digits.
-Output Format
-
-If  is a prime number, print prime; otherwise, print not prime.
-
-Sample Input
-
-13
-Sample Output
-
-prime
-Explanation
-
-The only positive divisors of  are  and , so we print prime.
-
-
+/**
+ * @author aruns
+ * 
+ * Primality Test
+ * https://www.hackerrank.com/challenges/java-primality-test/problem
+ *
  */
 public class PrimalityTest {
 	
@@ -37,10 +17,10 @@ public class PrimalityTest {
 
     public static void main(String[] args) {
         String n = scanner.nextLine();
-        BigInteger l = new BigInteger(n);
+        BigInteger input = new BigInteger(n);
         
 
-        System.out.println(l.isProbablePrime(1)?"prime":"not prime");
+        System.out.println(input.isProbablePrime(10)?"prime":"not prime");
         scanner.close();
     }
 
